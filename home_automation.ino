158b1b22bc496aa6c0430f48b09a6b8741c5afd1
Chukka003 <151235544+Chukka003@users.noreply.github.com>
2024-05-09 23:40:36 +0530

    Add files via upload

---
+++ home_automation.ino
@@ -0,0 +1,66 @@
+#include <dht.h>
+
+dht DHT;                // Creates a DHT object
+
+char val;
+char val2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(4, INPUT);
+  pinMode(10, OUTPUT);
+
+  Serial.begin(9600);
+  digitalWrite(7, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(10, LOW);
+}
+
+void loop() {
+
+    if (val == '1') {
+    digitalWrite(7, LOW);
+    Serial.println("Light On");
+  } else if (val == '2') {
+    digitalWrite(7, HIGH);
+    Serial.println("Light Off");
+  } else if (val == '3') {
+    digitalWrite(6, LOW);
+    Serial.println("Fan On");
+  } else if (val == '4') {
+    digitalWrite(6, HIGH);
+    Serial.println("Fan Off");
+  }
+
+  int readData = DHT.read11(8);
+
+  float t = DHT.temperature;        // Read temperature
+  float h = DHT.humidity;           // Read humidity
+
+  Serial.print("Temperature = ");
+  Serial.print(t);
+  Serial.print("°C | ");
+  Serial.print("Humidity = ");
+  Serial.print(h);
+  Serial.println("% ");
+  Serial.println("");
+
+  int flameValue = digitalRead(4);
+  if (flameValue == LOW) {
+    Serial.println("Flame detected!");
+    digitalWrite(10, HIGH);
+  } else {
+    digitalWrite(10, LOW);
+  }
+
+   if (Serial.available()) {
+    val = Serial.read();
+    val2 = Serial.read();
+  }
+
+  delay(1500); 
+
+}
+
